796a3c5ee0a66f050392f65352747238856b487e
Zadio <zadikouadioelie@gmail.com>
2026-06-30 16:49:47 +0200

    debug: afficher detail erreur 500

--- server/index.js
+++ server/index.js
@@ -6795,7 +6795,7 @@ async function handleRequest(req, res) {
         await r.handler(req, res, params, body, parsed.query);
       } catch (e) {
         console.error(e);
-        sendJSON(res, 500, { error: "Erreur serveur." });
+        sendJSON(res, 500, { error: "Erreur serveur.", detail: e.message });
       }
       return;
     }
